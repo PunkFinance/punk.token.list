4df98350fc7ebe4d002bce838f7824e59286918c
PunkFinance <hello@punk.finance>
2021-05-10 12:11:41 +0900

    Update token.list.json & Update Validator Logging

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
+import consola from 'consola'
 import Ajv from "ajv"
 import Axios from 'axios'
+import FS from 'fs'
 
+consola.start( "Validate Scheme, Token List For Punk.Protocol" )
+console.log("")
+consola.info( "Download Validate Scheme" )
 Axios.get( "https://uniswap.org/tokenlist.schema.json" ).then( response=>{
 
     if( response.status === 200 ){
-        
+        consola.success( "1. Downloaded Validate Scheme" )
+
         const schema = response.data
 
-        import( './token.list.json' ).then( (jsonFile)=>{
-
-            const tokenList = jsonFile.default
-            const validate = new Ajv().compile(schema)
-            const valid = validate(tokenList)
-
-            console.log( "Json Validate : ", valid )
-        })
-
+        consola.info( "Read And token.list.json file to json Data" )
+        const jsonFile = FS.readFileSync('token.list.json', 'utf8');
+        consola.success( "1. Success read file" )
+        const tokenList = JSON.parse(jsonFile);
+        consola.success( "2. Success File to Json Data" )
+        
+        consola.info( "Loading ajv & compile scheme" )
+        const validate = new Ajv().compile(schema)
+        const valid = validate(tokenList)
+        consola.success( "1. Success Complie Scheme" )
+        
+        if( valid ){
+            console.log("")
+            consola.log( "Json Validate : ", valid )
+        }else{
+            console.log("")
+            consola.error( "Json Validate : ", valid )
+        }
     }
 
 } ).catch( e=>{
     
-    console.log( "Failed" )
+    consola.error( "Failed" )
 
 } )
 
